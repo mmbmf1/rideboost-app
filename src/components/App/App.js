ad99d0fe99ffe389e53dc119d4cf0971be41d0d8
Michael <mmbmf.mm@gmail.com>
2020-03-19 12:02:01 -0500

    initial context commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import RideBoostContext from "../../contexts/RideBoostContext";
 import Header from "../Header/Header";
 import LandingPage from "../LandingPage/LandingPage";
 import Footer from "../Footer/Footer";
 import Content from "../../content";
+import SignupPage from "../../routes/SingupPage/SignupPage";
 
 export default class App extends React.Component {
+  static contextType = RideBoostContext;
+
   constructor() {
     super();
     this.state = {
@@ -15,11 +20,14 @@ export default class App extends React.Component {
   render() {
     const content = this.state.content;
     return (
-      <div>
+      <RideBoostContext.Provider content={content}>
         <Header content={content} />
-        <LandingPage content={content} />
+        <Switch>
+          <Route exact path={"/"} component={LandingPage} />
+          <Route exact path={"/signup"} component={SignupPage} />
+        </Switch>
         <Footer content={content} />
-      </div>
+      </RideBoostContext.Provider>
     );
   }
 }
--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 function SignupForm() {
-  return <div>Hello Singup Form!</div>;
+  return (
+    <form>
+      <label for="first_name">First Name:</label>
+      <input type="text" name="first_name" />
+      <label for="last_name">Last Name:</label>
+      <input type="text" name="last_name" />
+      <label for="password">Password:</label>
+      <input type="password" name="password" />
+      <label for="email">Email Address:</label>
+      <input type="email" name="email" />
+      <label for="phone">Phone Number:</label>
+      <input type="tel" name="phone" />
+      <label for="location">Location:</label>
+      <input type="text" name="location" />
+      <input type="submit" value="Sign Up!" />
+    </form>
+  );
 }
 
 export default SignupForm;
