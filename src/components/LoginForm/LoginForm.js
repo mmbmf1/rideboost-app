b3954b84ee8ea33aafbf77fff02aa3f217483799
Michael <mmbmf.mm@gmail.com>
2020-03-19 12:56:07 -0500

    fixing for typo

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,9 +3,9 @@ import React from "react";
 function LoginForm() {
   return (
     <form>
-      <label for="email">Email Address:</label>
+      <label htmlFor="email">Email Address:</label>
       <input type="email" name="email" />
-      <label for="password">Password:</label>
+      <label htmlFor="password">Password:</label>
       <input type="password" name="password" />
       <input type="submit" value="Login" />
     </form>
