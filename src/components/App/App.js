cd608054aa55b7b5db921b21edc44d027140db72
Michael <mmbmf.mm@gmail.com>
2020-03-19 08:49:44 -0500

    creating context files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import LandingPage from "../LandingPage/LandingPage";
+import RideBoostContext from "../../RideBoostContext";
 
 export default class App extends React.Component {
+  static contextType = RideBoostContext;
+
   render() {
     return <LandingPage />;
   }
