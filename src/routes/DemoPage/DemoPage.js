6c87634cf56a24208566bd2a418fac8931f4f195
Michael <mmbmf.mm@gmail.com>
2020-03-19 13:11:57 -0500

    working test

--- src/routes/DemoPage/DemoPage.js
+++ src/routes/DemoPage/DemoPage.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Dashboard from "../../components/Dashboard/Dashboard";
 
 function DemoPage() {
-  return <div>Hello Demo Page!</div>;
+  return <Dashboard />;
 }
 
 export default DemoPage;
