57aad1de5d99ce70e4a978561495b92287c90709
Michael <mmbmf.mm@gmail.com>
2020-03-19 10:41:46 -0500

    not ready for context

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Header from "../Header/Header";
 import LandingPage from "../LandingPage/LandingPage";
-import RideBoostContext from "../../RideBoostContext";
 import Content from "../../content";
 
 export default class App extends React.Component {
-  static contextType = RideBoostContext;
-
   constructor() {
     super();
     this.state = {
