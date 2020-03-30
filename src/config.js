480811ac365d736d4b7dadb8df458b5fb916e974
Michael <mmbmf.mm@gmail.com>
2020-03-30 18:43:14 -0500

    api endpoint update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
-  // API_ENDPOINT: "",
+  // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://peaceful-coast-56011.herokuapp.com/api",
   TOKEN_KEY: "rideboost-app-auth-token",
   USER_ID: "rideboost-user"
 };
