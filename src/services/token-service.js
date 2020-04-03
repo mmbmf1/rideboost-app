1d2998c4208b679137885cd81a89b3bb56e83c4e
Michael <mmbmf.mm@gmail.com>
2020-04-03 11:23:52 -0500

    update to session storage

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -2,19 +2,23 @@ import config from "../config";
 
 const TokenService = {
   saveAuthToken(token) {
-    window.localStorage.setItem(config.TOKEN_KEY, token);
+    window.sessionStorage.setItem(config.TOKEN_KEY, token);
+    // window.localStorage.setItem(config.TOKEN_KEY, token);
   },
 
   saveUserId(user_id) {
-    window.localStorage.setItem(config.USER_ID, user_id);
+    window.sessionStorage.setItem(config.USER_ID, user_id);
+    // window.localStorage.setItem(config.USER_ID, user_id);
   },
 
   getUserId() {
-    return window.localStorage.getItem(config.USER_ID);
+    return window.sessionStorage.getItem(config.USER_ID);
+    // return window.localStorage.getItem(config.USER_ID);
   },
 
   getAuthToken() {
-    return window.localStorage.getItem(config.TOKEN_KEY);
+    return window.sessionStorage.getItem(config.TOKEN_KEY);
+    // return window.localStorage.getItem(config.TOKEN_KEY);
   },
 
   hasAuthToken() {
@@ -22,12 +26,14 @@ const TokenService = {
   },
 
   clearAuthToken() {
-    window.localStorage.removeItem(config.TOKEN_KEY);
+    window.sessionStorage.removeItem(config.TOKEN_KEY);
+    // window.localStorage.removeItem(config.TOKEN_KEY);
   },
 
   clearUserId() {
-    window.localStorage.removeItem(config.USER_ID);
-  }
+    window.sessionStorage.removeItem(config.USER_ID);
+    // window.localStorage.removeItem(config.USER_ID);
+  },
 };
 
 export default TokenService;
